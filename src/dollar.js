dd82e92d24b30cb97ccc0888429dcbe8faa9999f
509Dave16 <davidfall@rocketmail.com>
2016-06-09 13:32:28 -0700

    test(times): Wrote failing test, stubbed Dollar, and then implemented details for 'times' in Dollar

--- src/dollar.js
+++ src/dollar.js
@@ -1,3 +1,17 @@
 /**
  * Code goes here for program or package.
  */
+export default (amount) => {
+  const times = (value) => {
+    amount *= value;
+  };
+  
+  const getAmount = () => amount;
+
+
+
+  return {
+    getAmount,
+    times
+  }
+}
--- test/dollar.js
+++ test/dollar.js
@@ -2,12 +2,19 @@
  * Tests go here for testing program or package.
  */
 
-import Dolloar from '../src/dollar';
-
+import createDollar from '../src/dollar';
+import {expect} from 'chai';
 /**
- * 1) $5 + 10 CHF = $10 if rate is 2:1
- * 2) $5 * 2 = $10
+ *  1) $5 + 10 CHF = $10 if rate is 2:1
+ * X2) $5 * 2 = $10
+ * X3)Make "amount" private
+ *  4)Dollar side-effects?
+ *  5)Money rounding?
  */
 describe('Dollar', () => {
-
+  it('times', () => {
+    const five = createDollar(5);
+    five.times(2);
+    expect(five.getAmount()).to.equal(10);
+  });
 });
